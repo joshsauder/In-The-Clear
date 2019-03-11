4f572299aacf40eb2077dfa535e0304a142560d4
joshsauder <joshsauder@gmail.com>
2019-03-10 22:04:46 -0400

    Created new Unit Test Cases

---
+++ InTheClearTests.swift
@@ -0,0 +1,58 @@
+//
+//  InTheClearTests.swift
+//  InTheClearTests
+//
+//  Created by Josh Sauder on 3/10/19.
+//  Copyright © 2019 Josh Sauder. All rights reserved.
+//
+
+import XCTest
+
+class InTheClearTests: XCTestCase {
+
+    /*
+     Test that will input two locations that you can drive to and from.
+     Should return a valid route
+    */
+    func testNormalRoute() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    /*
+     Test that will input two locations that you cannot drive to and from
+     Should return nothing, and should not error out
+    */
+    func testOverseasRoute(){
+        
+    }
+
+    /*
+     Test that will check to ensure line is being drawn correctly
+    */
+    func testRouteLine(){
+        
+    }
+    
+    /*
+     Test that will input a second route and will ensure first route is removed
+    */
+    func testSecondRoute(){
+        
+    }
+    
+    /*
+     Ensures correct route time is being displayed
+    */
+    func testTotalTime(){
+        
+    }
+    
+    /*
+     Test that ensures correct times are being pulled from weather service with respect to the time at which the driver will pass through
+    */
+    func testCorrectTimes(){
+        
+    }
+
+}
---
+++ WeatherTableTests.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherTableTests.swift
+//  InTheClearTests
+//
+//  Created by Josh Sauder on 3/10/19.
+//  Copyright © 2019 Josh Sauder. All rights reserved.
+//
+
+import XCTest
+
+class WeatherTableTests: XCTestCase {
+    
+    /*
+     Test to ensure correct color and image are being displayed
+ ```*/
+    func testTableCell() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    /*
+     Test to ensure correct number of table values are being displayed
+    */
+    func testTableSize(){
+        
+    }
+    
+    /*
+     Test to ensure correct conditions/temps are being displayed with each city
+    */
+    func testValueCorrectness(){
+        
+    }
+    
+    /*
+     Test a new route being inputed. Should remove old route values and only include new
+    */
+    func testSecondRoute(){
+        
+    }
+}
