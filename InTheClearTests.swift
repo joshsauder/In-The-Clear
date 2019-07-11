cf2c9d7b1288e0a3ac2c163e96a987a700570cdd
Josh Sauder <joshsauder@gmail.com>
2019-07-10 22:36:13 -0400

    updated test cases

--- InTheClearTests.swift
+++ InTheClearTests.swift
@@ -10,6 +10,7 @@ import XCTest
 @testable import InTheClear
 import GoogleMaps
 import SwiftyJSON
+import Alamofire
 
 class InTheClearTests: XCTestCase {
     
@@ -25,7 +26,7 @@ class InTheClearTests: XCTestCase {
 
     /*
      Test that will input two locations that you can drive to and from.
-     Should return a valid route
+     Should return a valid route and a time and distance value
     */
     func testNormalRoute() {
         
@@ -35,9 +36,11 @@ class InTheClearTests: XCTestCase {
         //Columbus, OH coordinates
         let destination = CLLocation(latitude: CLLocationDegrees(exactly: 39.96199)!, longitude: CLLocationDegrees(exactly: -83.00275)!)
         
-        vc.createLine(startLocation: start, endLocation: destination) {condition in
+        vc.createLine(startLocation: start, endLocation: destination) {time, distance in
             //if there is a valid route, the associated polyline will be added to the polyline array
             assert(self.vc.polylineArray.count > 0)
+            assert(time != "")
+            assert(distance != "")
         }
         
         
@@ -55,7 +58,7 @@ class InTheClearTests: XCTestCase {
         //Los Angeles, CA coordinates
         let destination = CLLocation(latitude: CLLocationDegrees(exactly: 34.05349)!, longitude: CLLocationDegrees(exactly: -118.24532)!)
         
-        vc.createLine(startLocation: start, endLocation: destination){ condition in
+        vc.createLine(startLocation: start, endLocation: destination){ time, distance in
             assert(self.vc.polylineArray.count == 0)
         }
         
@@ -72,7 +75,7 @@ class InTheClearTests: XCTestCase {
         //Columbus, OH coordinates
         let destination = CLLocation(latitude: CLLocationDegrees(exactly: 39.96199)!, longitude: CLLocationDegrees(exactly: -83.00275)!)
         
-        vc.createLine(startLocation: start, endLocation: destination){ condition in
+        vc.createLine(startLocation: start, endLocation: destination){ time, distance in
             let path = self.vc.polylineArray[0].path
             
             //get coordinates
@@ -101,7 +104,7 @@ class InTheClearTests: XCTestCase {
         //Columbus, OH coordinates
         let destination = CLLocation(latitude: CLLocationDegrees(exactly: 39.96199)!, longitude: CLLocationDegrees(exactly: -83.00275)!)
         
-        vc.createLine(startLocation: start, endLocation: destination){ condition in
+        vc.createLine(startLocation: start, endLocation: destination){ time, distance in
             assert(self.vc.polylineArray.count == 1)
         }
         
@@ -111,11 +114,92 @@ class InTheClearTests: XCTestCase {
         //Columbus, OH coordinates
         let secondDestination = CLLocation(latitude: CLLocationDegrees(exactly: 32.77815)!, longitude: CLLocationDegrees(exactly: -96.7954)!)
         
-        vc.createLine(startLocation: secondStart, endLocation: secondDestination){condition in
+        vc.createLine(startLocation: secondStart, endLocation: secondDestination){time, distance in
             assert(self.vc.polylineArray.count == 1)
         }
         
         //if there is a valid route, the associated polyline will be added to the polyline array
+    }
+    
+    /**
+     Test Weather Lambda Function
+     Should return weather data for two cities. Also checks to ensure time is added correctly
+    */
+    func testWeatherFunction(){
+        //San Diego and Oceanside coordinates
+        let latArray:[String] = ["32.715904","33.192065"]
+        let longArray:[String] = ["-117.167608", "-117.376427"]
+        //10 minutes and 11 minutes in the future
+        let timeArray:[Int] = [600, 660]
+        
+        let refDate = Date().timeIntervalSince1970
+        let timeInterval = TimeInterval(refDate)
+        var date = Date(timeIntervalSince1970: timeInterval)
+        
+        let testDate = timeInterval
+        
+        var weatherArray: [[String: Any]] = []
+        
+        var i = 0
+        //create dictionary array for request
+        while i < latArray.count {
+            var dictionaryItem: [String: Any] = [:]
+            
+            dictionaryItem["lat"] = latArray[i]
+            dictionaryItem["long"] = longArray[i]
+            
+            date = date.addingTimeInterval(TimeInterval(timeArray[i]))
+            dictionaryItem["time"] = date.timeIntervalSince1970.rounded()
+            
+            weatherArray.append(dictionaryItem)
+            i = i + 1
+        }
+        
+        let weatherParameters: Parameters = ["List" : weatherArray]
+        
+        vc.getWeather(parameters: weatherParameters){ json in
+            //assert coordinates match up
+            assert(json[0]["lat"] as! String == latArray[0] && json[0]["long"] as! String == longArray[0])
+            assert(json[1]["lat"] as! String == latArray[1] && json[1]["long"] as! String == longArray[1])
+            //assert weather details exist
+            assert((json[0]["condition"] as! String).count > 0 && (json[0]["temp"] as! String).count > 0)
+            assert((json[1]["condition"] as! String).count > 0 && (json[1]["temp"] as! String).count > 0)
+            //assert requestedtime is added correctly to current time
+            assert(weatherArray[0]["time"] as! Int == Int(testDate) + timeArray[0])
+            assert(weatherArray[1]["time"] as! Int == Int(testDate) + timeArray[1])
+        }
+    }
+    
+    /**
+     Test Reverse Geocode Lambda function
+     Expect two cities to be returned
+    */
+    func testReverseGeolocatingFunction(){
+        //San Diego and Oceanside coordinates
+        let latArray:[String] = ["32.715904","33.192065"]
+        let longArray:[String] = ["-117.167608", "-117.376427"]
+        
+        var locationArray: [[String: Any]] = []
+        
+        var i = 0
+        //create dictionary array for request
+        while i < latArray.count {
+            var dictionaryItem: [String: Any] = [:]
+            
+            dictionaryItem["lat"] = latArray[i]
+            dictionaryItem["long"] = longArray[i]
+            
+            locationArray.append(dictionaryItem)
+            i = i + 1
+        }
+        
+        let weatherParameters: Parameters = ["List" : locationArray]
+        
+        vc.getLocationName(parameters: weatherParameters){
+            //assert both cities are added to cities array
+            assert(self.vc.cities[0] == "San Diego, CA")
+            assert(self.vc.cities[1] == "Oceanside, CA")
+        }
     }
 
 }
--- WeatherTableTests.swift
+++ WeatherTableTests.swift
@@ -8,7 +8,6 @@
 
 import XCTest
 @testable import InTheClear
-import FontAwesome_swift
 import GoogleMaps
 
 class WeatherTableTests: XCTestCase {
@@ -25,11 +24,11 @@ class WeatherTableTests: XCTestCase {
     func testTableCellColor() {
         
         //actual values
-        let rain = wm.cellColor(weather: "Rain")
-        let sun = wm.cellColor(weather: "Sun")
-        let snow = wm.cellColor(weather: "Snow")
-        let ts = wm.cellColor(weather: "Thunderstorm")
-        let cloud = wm.cellColor(weather: "Clouds")
+        let rain = wm.cellColor(weather: "rain")
+        let sun = wm.cellColor(weather: "sun")
+        let snow = wm.cellColor(weather: "snow")
+        let ts = wm.cellColor(weather: "danger")
+        let cloud = wm.cellColor(weather: "clouds")
         
         //expected values
         let expectedRain = [UIColor(red:0.40, green:0.73, blue:0.42, alpha:1.0), UIColor(red:0.26, green:0.63, blue:0.28, alpha:1.0)]
@@ -59,7 +58,7 @@ class WeatherTableTests: XCTestCase {
         //Sacramento, CA coordinates
         let destination = CLLocation(latitude: CLLocationDegrees(exactly: 38.5816)!, longitude: CLLocationDegrees(exactly: 121.4944)!)
         
-        vc.createLine(startLocation: start, endLocation: destination){ condition in
+        vc.createLine(startLocation: start, endLocation: destination){ time, distance in
             
             //Miami, FL coordinates
             let secondStart = CLLocation(latitude: CLLocationDegrees(exactly: 25.77481)!, longitude: CLLocationDegrees(exactly: -80.19773)!)
@@ -67,7 +66,7 @@ class WeatherTableTests: XCTestCase {
             //Columbus, OH coordinates
             let secondDestination = CLLocation(latitude: CLLocationDegrees(exactly: 32.77815)!, longitude: CLLocationDegrees(exactly: -96.7954)!)
             
-            vc.createLine(startLocation: secondStart, endLocation: secondDestination){ condition in
+            vc.createLine(startLocation: secondStart, endLocation: secondDestination){ time, distance in
                 var test = true
                 
                 //check for the first routes first and last cities
