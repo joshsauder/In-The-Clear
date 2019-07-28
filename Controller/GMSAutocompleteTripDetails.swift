ffc128ccd5ca0eb16351dbd9bfe2f95625463a28
Josh Sauder <joshsauder@gmail.com>
2019-07-27 20:35:33 -0400

    added gms autocomplete and cell to trip details

---
+++ Controller/GMSAutocompleteTripDetails.swift
@@ -0,0 +1,37 @@
+//
+//  GMSAutocompleteTripDetails.swift
+//  InTheClear
+//
+//  Created by Josh Sauder on 7/27/19.
+//  Copyright © 2019 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
+import UIKit
+import GooglePlaces
+
+extension CustomizeTripDetails: GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        //add city to cities array
+        cities.append(place.name!)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: \(error)")
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+}
---
+++ Controller/TripDetailsTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  TripDetailsTableViewCell.swift
+//  InTheClear
+//
+//  Created by Josh Sauder on 7/26/19.
+//  Copyright © 2019 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TripDetailsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var CityName: UILabel!
+    @IBOutlet weak var DatePicker: UIDatePicker!
+    @IBOutlet weak var CancelButton: UIButton!
+    @IBOutlet weak var MoveButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        createDatePicker()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    /**
+     Configure the date picker
+     */
+    func createDatePicker(){
+        
+        DatePicker.datePickerMode = UIDatePickerMode.dateAndTime
+        
+        //set min and max date values for date picker
+        let minDate = Calendar.current.date(byAdding: .day, value: .zero, to: Date())
+        let maxDate = Calendar.current.date(byAdding: .day, value: 3, to: Date())
+        DatePicker.minimumDate = minDate
+        DatePicker.maximumDate = maxDate
+        
+    }
+}
