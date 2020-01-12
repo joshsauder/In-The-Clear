a9aada88522899c7e2250d572fc89901e9bb742e
Josh Sauder <joshsauder@gmail.com>
2020-01-12 16:27:54 -0500

    added login and register views

---
+++ Controller/LoginController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginController.swift
+//  InTheClear
+//
+//  Created by Josh Sauder on 1/12/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
