f547578ee8a0efd549f4803e4fb396d124a43122
Josh Sauder <joshsauder@gmail.com>
2020-01-12 20:06:23 -0500

    sign in button works

--- Controller/LoginController.swift
+++ Controller/LoginController.swift
@@ -8,14 +8,60 @@
 
 import Foundation
 import UIKit
+import AuthenticationServices
 
-
-class ViewController: UIViewController {
+class LoginController: UIViewController, ASAuthorizationControllerDelegate {
+    
+    @IBOutlet weak var InputView: UIView!
+    @IBOutlet weak var AppleButtonView: UIStackView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpSignInAppleButton()
     }
     
+    func setUpSignInAppleButton() {
+        
+        let authButton = ASAuthorizationAppleIDButton()
+        
+        authButton.addTarget(self, action: #selector(handleAppleAuth), for: .touchUpInside)
+        
+        self.AppleButtonView.addArrangedSubview(authButton)
+    }
     
+    @objc func handleAppleAuth(){
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as?  ASAuthorizationAppleIDCredential {
+            
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            print("\(userIdentifier) \(String(describing: fullName)) \(String(describing: email))")
+        
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        
+        print(error)
+    }
+
+}
+
+extension LoginController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
 }
