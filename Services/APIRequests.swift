1106447258060a60319e0e306c9ce604bd74c816
Josh Sauder <joshsauder@gmail.com>
2020-01-12 23:15:24 -0500

    added login routes

--- Services/APIRequests.swift
+++ Services/APIRequests.swift
@@ -99,4 +99,13 @@ extension ViewController {
         }
     }
     
+    func signInUser(url: String, completion: @escaping (Int?) -> ()){
+        
+        Alamofire.request(url, method: .post).validate().responseJSON {
+            response in
+            
+            completion(response.response?.statusCode)
+        }
+    }
+    
 }
