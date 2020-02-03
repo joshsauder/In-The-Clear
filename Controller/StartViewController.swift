18d9c9d6ed42b58bfb88e91d9228508bcb91b19f
Josh Sauder <joshsauder@gmail.com>
2020-02-02 21:29:17 -0500

    added new storyboards for login

---
+++ Controller/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  InTheClear
+//
+//  Created by Josh Sauder on 2/2/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
