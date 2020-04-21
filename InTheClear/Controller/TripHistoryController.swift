c07521165c462b103a0dbb062a88edb8a8a58a21
Josh Sauder <joshsauder@gmail.com>
2020-04-20 22:27:12 -0400

    setup trip table

--- InTheClear/Controller/TripHistoryController.swift
+++ InTheClear/Controller/TripHistoryController.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Josh Sauder. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TripHistoryController: UITableViewController {
+    
+    var tripDataArray: [TripHistory] = []
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.separatorStyle = .none
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tripDataArray.count
+    }
+    
+    
+}
---
+++ InTheClear/Model/TripHistory.swift
@@ -0,0 +1,18 @@
+//
+//  TripHistory.swift
+//  InTheClear
+//
+//  Created by Josh Sauder on 4/20/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+
+class TripHistory {
+    
+    var OverviewLabel: String
+    
+    init(overviewLabel: String){
+        self.OverviewLabel = overviewLabel
+    }
+}
